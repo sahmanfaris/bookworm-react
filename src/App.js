fca32760630df47bf503dc68ad5727d40ad10b72
@farissahman1 <farissahman1@gmail.com>
2020-09-23 21:47:15 +0200

    login form with validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-const App = () => <div>Esselamu alejkum</div>;
+const App = () => (
+  <div className="ui container">
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  </div>
+);
 
 export default App;
